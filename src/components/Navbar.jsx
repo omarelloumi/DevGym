b9b253f5f5a36b163d368350ced99a76ba707623
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-08 06:01:25 +0100

    Create home structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { Stack } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
 const Navbar = () => {
   return (
-    <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+    <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }}} px="20px">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+      <img src={Logo} alt="logo" style={{ width: '50px', height: '50px', margin: '0px 20px' }} />
     </Link>
     <Stack
       direction="row"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
 
