b3192b60dd185d095c5b411056c5491378896ae0
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-08 09:13:08 +0100

    Create static search component

--- src/components/SearchExercises/SearchExercises.jsx
+++ src/components/SearchExercises/SearchExercises.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState('')
+  const handleSearch = (e) => {
+  }
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
