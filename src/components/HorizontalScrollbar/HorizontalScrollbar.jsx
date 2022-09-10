7154af43050ff79c0e058362fe90759c758738a5
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-10 10:31:17 +0100

    add arrows to scroll between categories

--- src/components/HorizontalScrollbar/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar/HorizontalScrollbar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import { Box } from '@mui/material';
+import React,{ useContext } from 'react'
+import { Box, Typography } from '@mui/material';
 import BodyPart from '../BodyPart/BodyPart';
-import { ScrollMenu} from 'react-horizontal-scrolling-menu';
+import { ScrollMenu,VisibilityContext} from 'react-horizontal-scrolling-menu';
+import RightArrowIcon from '../../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../../assets/icons/left-arrow.png';
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => {
 
+  const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollPrev()} className="right-arrow">
+        <img src={LeftArrowIcon} alt="right-arrow" />
+      </Typography>
+    );
+  };
+  
+  const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollNext()} className="left-arrow">
+        <img src={RightArrowIcon} alt="right-arrow" />
+      </Typography>
+    );
+  };
+
   return (
-    <ScrollMenu>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} >
     {data.map((item) => (
         <Box
           key={item.id || item}
-          itemId={item.id || item}
+          itemID={item.id || item}
           title={item.id || item}
           m="0 40px"
         >
